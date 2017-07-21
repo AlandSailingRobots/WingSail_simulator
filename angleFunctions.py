38081b299ecd679c8e081a146311ec53ac1f2606
Jazicoan <julien.damers@ensta-bretagne.org>
2017-07-21 13:44:28 +0300

    added pics directory path

---
+++ angleFunctions.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 21 11:02:38 2017
+
+@author: jazicoan
+"""
+import numpy as np
+
+def degTorad(angle):
+	return((angle*np.pi/180))
+
+	
+def radTodeg(angle):
+	return(angle*180/np.pi)
+
+def wrapTo2pi(theta):
+	theta = 2.0*np.arctan(np.tan(theta/2.0))
+	return(theta)
+
+
+print(degTorad(5))
+print(degTorad(10))
+print(degTorad(15))
+print(degTorad(20))
+print(degTorad(25))
+print(degTorad(30))
